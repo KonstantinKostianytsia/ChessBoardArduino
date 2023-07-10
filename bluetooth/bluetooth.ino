16f0a443af0a8e1d352d0eab9e25f57d46d37714
Kostiantyn Kostianytsia <konstantin.kostianytsia@nixsolutions.com>
2023-07-10 10:22:50 +0300

    Implemented hall sensors sending

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -4,37 +4,52 @@
 #include <BLE2902.h>
 #include <FastLED.h>
 
+// Common constants
+#define NUM_CELLS_IN_ROW       8 
+#define NUM_ROWS               8
+#define SEND_BOARD_STATE_DELAY 10000
 
 // Parser constants
 #define CHARACTER_TO_SEPARATE_COMMANDS  ';'
 #define CHARACTER_TO_SEPARATE_FIELDS    '_'
 
 // BLE constants
-#define SERVICE_UUID        "5f47f8ff-fbb4-47d4-ac92-8520ef9fed17"
-#define WRITE_COLOR_CHAR    "17e8a436-de30-47dd-bfb8-baf4d82afbdb"
-// #define CHAR2_UUID          "7d09ef8c-b14f-4bbb-9d48-4b1c1f7f8044"
+#define SERVICE_UUID                        "5f47f8ff-fbb4-47d4-ac92-8520ef9fed17"
+#define WRITE_COLOR_CHAR                    "17e8a436-de30-47dd-bfb8-baf4d82afbdb"
+#define BOARD_STATE_NOTIFICATIONS_CHAR      "7d09ef8c-b14f-4bbb-9d48-4b1c1f7f8044"
 
 // LEDs constants
-int BOARD_ROW_LED_PINS[8] = {4, 5, -1, -1, -1, -1, -1, -1};
-#define NUM_LEDS_IN_ROW 8 // кількість діодів у рядку стрічці
+int BOARD_ROW_LED_PINS[NUM_ROWS] = {4, 5, -1, -1, -1, -1, -1, -1};
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 #define BRIGHTNESS 250
 
+/// Hall sensors constats
+#define ANALOG_READ_RESOLUTION      10
+#define s0Pin                       26
+#define s1Pin                       27
+#define s2Pin                       14
+#define s3Pin                       12
+#define s4Pin                       -1
+#define s5Pin                       -1
+
+#define analogPin                   25 /// read only
+
+
 // FastLEDs variables
-CRGB leds[8][NUM_LEDS_IN_ROW];
+CRGB leds[NUM_ROWS][NUM_CELLS_IN_ROW];
 
 // BLE variables
 BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
-// BLECharacteristic* pCharacteristic_2 = NULL;
+BLECharacteristic* pCharacteristicWriteColor = NULL;
+BLECharacteristic* pCharacteristicBoardState = NULL;
 BLEDescriptor *pDescr;
 BLE2902 *pBLE2902;
 
 // other variables
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint32_t value = 0;
+
 
 class BLEServerConnectCallback: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -113,15 +128,16 @@ class BLEOnWriteColorCallback: public BLECharacteristicCallbacks {
   }
 };
 
-void setup() {
-  Serial.begin(115200);
+void setupLeds() {
   pinMode(LED_BUILTIN, OUTPUT);
 
-  FastLED.addLeds<LED_TYPE, 4, COLOR_ORDER>(leds[0], NUM_LEDS_IN_ROW);
-  FastLED.addLeds<LED_TYPE, 5, COLOR_ORDER>(leds[1], NUM_LEDS_IN_ROW);
+  FastLED.addLeds<LED_TYPE, 4, COLOR_ORDER>(leds[0], NUM_CELLS_IN_ROW);
+  FastLED.addLeds<LED_TYPE, 5, COLOR_ORDER>(leds[1], NUM_CELLS_IN_ROW);
   FastLED.setBrightness(BRIGHTNESS);
+}
 
-  // Create the BLE Device
+void setupBLEService() {
+    // Create the BLE Device
   BLEDevice::init("ESP32_CHESS");
 
   // Create the BLE Server
@@ -132,13 +148,25 @@ void setup() {
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
   // Create a BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
+  pCharacteristicWriteColor = pService->createCharacteristic(
                       WRITE_COLOR_CHAR,
                       BLECharacteristic::PROPERTY_WRITE  
                     );  
+  pCharacteristicBoardState = pService->createCharacteristic(
+                      BOARD_STATE_NOTIFICATIONS_CHAR,
+                      BLECharacteristic::PROPERTY_NOTIFY  
+                    );  
+                  
+  pDescr = new BLEDescriptor((uint16_t)0x2901);
+  pCharacteristicBoardState->addDescriptor(pDescr);
+
+  pBLE2902 = new BLE2902();
+  pBLE2902->setNotifications(true);
+
+  pCharacteristicBoardState->addDescriptor(pBLE2902);
 
   // After defining the desriptors, set the callback functions
-  pCharacteristic->setCallbacks(new BLEOnWriteColorCallback());
+  pCharacteristicWriteColor->setCallbacks(new BLEOnWriteColorCallback());
 
   // Start the service
   pService->start();
@@ -151,13 +179,76 @@ void setup() {
   pAdvertising->setMinPreferred(0x00);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
   Serial.println("Waiting a client connection to notify...");
-
 }
+
+void setupHallSensors() {
+  /// Initialize pins to control multiplexor
+  pinMode(s0Pin, OUTPUT);
+  pinMode(s1Pin, OUTPUT);
+  pinMode(s2Pin, OUTPUT);
+  pinMode(s3Pin, OUTPUT);
+  pinMode(s4Pin, OUTPUT);
+  pinMode(s5Pin, OUTPUT);
+
+  /// Set initial values
+  digitalWrite(s0Pin, LOW);
+  digitalWrite(s1Pin, LOW);
+  digitalWrite(s2Pin, LOW);
+  digitalWrite(s3Pin, LOW);
+  digitalWrite(s4Pin, LOW);
+  digitalWrite(s5Pin, LOW);
+
+  analogReadResolution(ANALOG_READ_RESOLUTION);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  setupLeds();
+  setupHallSensors();
+  setupBLEService();
+}
+
+void handleHallSensors() {
+  String commandString = "";
+  // for (int i = 0; i < NUM_CELLS_IN_ROW * NUM_ROWS; i++) {
+  for (int i = 0; i < 8; i++) {
+    if(i != 0) {
+      commandString.concat(CHARACTER_TO_SEPARATE_COMMANDS);
+    }
+    // Set values to controll input of the multiplexor
+    digitalWrite(s0Pin, i & 0x01);
+    digitalWrite(s1Pin, (i >> 1) & 0x01);
+    digitalWrite(s2Pin, (i >> 2) & 0x01);
+    digitalWrite(s3Pin, (i >> 3) & 0x01);
+    // digitalWrite(s4Pin, (i >> 4) & 0x01);
+    // digitalWrite(s5Pin, (i >> 5) & 0x01);
+
+    // A delay for signal stabilization
+    delay(10);
+
+    // Reading signal value
+    int sensorValue = analogRead(analogPin);
+    byte rowIndex = i / NUM_CELLS_IN_ROW;
+    byte columnIndex = i - (rowIndex * NUM_CELLS_IN_ROW);
+    
+    commandString.concat(rowIndex);
+    commandString.concat(CHARACTER_TO_SEPARATE_FIELDS);
+    commandString.concat(columnIndex);
+    commandString.concat(CHARACTER_TO_SEPARATE_FIELDS);
+    commandString.concat(sensorValue);
+  }
+  Serial.println(commandString.c_str());
+  pCharacteristicBoardState->setValue(commandString.c_str());
+  pCharacteristicBoardState->notify();
+}
+
 
 void loop() {
     // notify changed value
     if (deviceConnected) {
-        delay(1000);
+        handleHallSensors();
+        delay(SEND_BOARD_STATE_DELAY);
     }
      // disconnecting
     if (!deviceConnected && oldDeviceConnected) {
